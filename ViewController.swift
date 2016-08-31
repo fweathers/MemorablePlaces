d6e498c46aaf1beb481abb0edc381d9c76cae42f
Felicia Weathers <Fel.Weathers@gmail.com>
2016-08-30 22:29:38 -0400

    Set up tableViewController with bar button segue to Maps VC

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemorablePlaces
+//
+//  Created by Felicia Weathers on 8/30/16.
+//  Copyright © 2016 Felicia Weathers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
